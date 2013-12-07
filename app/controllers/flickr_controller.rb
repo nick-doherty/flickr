93ad2e154ec66b698682849a8760e1b305f583aa
Nick Doherty <nick@Nicks-iMac.local>
2013-12-07 19:54:33 +1100

    second commit

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -6,5 +6,12 @@ class FlickrController < ApplicationController
     FlickRaw.api_key='7859fb2987ac7a93a2ace28e19d1edbf'
     FlickRaw.shared_secret='e24116eb9a93f84a'
     @images = flickr.photos.search(:text => query)
+    binding.pry
+    @urls['flickr']=FlickRaw.url(@images)
   end
 end
+
+
+# http://www.flickr.com/services/api/misc.urls.html
+# http://www.flickr.com/services/api/flickr.photos.search.html
+# http://hanklords.github.io/flickraw/
