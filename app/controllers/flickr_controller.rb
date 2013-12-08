89c01a8710f09a1c112105adef2a44bf700c8f3f
Nick Doherty <nick@Nicks-iMac.local>
2013-12-08 20:05:42 +1100

    with bootstrap

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -9,7 +9,7 @@ class FlickrController < ApplicationController
     FlickRaw.api_key='7859fb2987ac7a93a2ace28e19d1edbf'
     FlickRaw.shared_secret='e24116eb9a93f84a'
     @images = flickr.photos.search(:text => query,
-                                   :per_page => 5,
+                                   :per_page => 12,
                                    :page => @page)
     # guard against flickr not returning any results
     if @images.count == 0
--- spec/controllers/flickr_controller_spec.rb
+++ spec/controllers/flickr_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe FlickrController do
   describe "show"  do
-    it "sets a search term"
-    it "sets a page number for the results"
-    it "returns 100 images from flickr"
-    it "displays a no results message if there are no results"
+    # it "sets a search term"
+    # it "sets a page number for the results"
+    # it "returns 100 images from flickr"
+    # it "displays a no results message if there are no results"
   end
 end
