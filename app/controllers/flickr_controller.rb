37373cd43650b75384f7db6860f3aed04c27289b
Nick Doherty <nickdoherty@gmail.com>
2013-12-08 12:57:47 +1100

    Link to search again

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -2,14 +2,16 @@ class FlickrController < ApplicationController
 
   def show
     query = params[:query]
+    # guard against an empty submission by returning something
     query = 'sydney' unless query.present?
     @page = params[:page] || 1
     @page = @page.to_i
     FlickRaw.api_key='7859fb2987ac7a93a2ace28e19d1edbf'
     FlickRaw.shared_secret='e24116eb9a93f84a'
     @images = flickr.photos.search(:text => query,
-                                   :per_page => 10,
+                                   :per_page => 5,
                                    :page => @page)
+    # guard against flickr not returning any results
     if @images.count == 0
       flash[:status] = '<p>No results found'
       redirect_to root_path
